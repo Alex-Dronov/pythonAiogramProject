9c4e300cba9f0eb61a093bb6d1935cb17c343980
Alex-Dronov <159939057+Alex-Dronov@users.noreply.github.com>
2024-12-13 12:19:34 +0300

    Module_13 homework

--- mail.py
+++ mail.py
@@ -1,10 +1,19 @@
-from aiogram import Bot, Dispatcher, executor, types
+from aiogram import Bot, Dispatcher, executor
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-import asyncio
 
-api = '8043575185:AAHXB03fBm7NxvJg0fwbS-WuDhYdoPMnDsE'
+
+api = ''
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
+
+@dp.message_handler(commands = ['start'])
+async def start(message):
+    print('Привет! Я бот помогающий твоему здоровью.')
+
+@dp.message_handler()
+async def all_massages(message):
+    print('Введите команду /start, чтобы начать общение.')
+
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
